4ffc07666d229b8b53631d047c527810dc828a43
leslieluyu <leslie.luyu@gmail.com>
2023-06-04 22:47:43 -0700

    1. modify the wait method in process_c2c_java.sh

--- scripts/process_c2c_java.sh
+++ scripts/process_c2c_java.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BIN_MAP="/home/ubuntu/yulu/perf-map-agent/bin/create-java-perf-map.sh"
+BIN_MAP="/home/ansible/yulu/perf-map-agent/bin/create-java-perf-map.sh"
 
 # Check if scenario argument is provided
 if [[ $# -ne 1 ]]; then
@@ -10,6 +10,7 @@ if [[ $# -ne 1 ]]; then
 fi
 
 scenario=$1
+out=${2:-"/home/ansible/yulu/cifs_data/luyu/false_sharing/grpc_java/spr/"}
 
 dt=`TZ='Asia/Shanghai' date "+%m%d%H%M%S"`
 c_id=`ps -ef | grep io.grpc.benchmarks.driver.LoadWorker | grep 10410 | awk '{print $2}'`
@@ -47,18 +48,21 @@ sudo perf c2c record -a -u --ldlat 50 -p $s_id -o ${scenario}_server_$dt -- slee
 pid2=$!
 
 # Wait for both commands to finish
-wait $pid1
-wait $pid2
+wait
+#wait $pid1
+#wait $pid2
 
+echo "sleep 20 seconds.... for perf record to finish "
+sleep 20
 # Process output files in parallel
 {
     echo "process the ${scenario}_client_${dt} "
-    sudo perf c2c report -NN -g --call-graph --full-symbols -c pid,iaddr --stdio -i ${scenario}_client_${dt} > ${scenario}_client_${dt}.txt
+    sudo perf c2c report -NN -g --call-graph --full-symbols -c pid,iaddr --stdio -i ${scenario}_client_${dt} > ${out}/${scenario}_client_${dt}.txt
 } &
 
 {
     echo "process the ${scenario}_server_${dt} "
-    sudo perf c2c report -NN -g --call-graph --full-symbols -c pid,iaddr --stdio -i ${scenario}_server_${dt}  > ${scenario}_server_${dt}.txt
+    sudo perf c2c report -NN -g --call-graph --full-symbols -c pid,iaddr --stdio -i ${scenario}_server_${dt}  > ${out}/${scenario}_server_${dt}.txt
 } &
 
 # Wait for both report commands to finish
