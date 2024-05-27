5347e474f9f34aec669d501356293f6b563ac081
leslieluyu <leslie.luyu@gmail.com>
2024-05-27 09:59:51 +0000

    1. rename the images of embedding,retriever,reranking,llm,backend 2. add remove_all for gaudi and xeon

--- genai_solution/GenAIInfra/Demo_ChatQnA/gaudi/remove_all_gaudi.sh
+++ genai_solution/GenAIInfra/Demo_ChatQnA/gaudi/remove_all_gaudi.sh
@@ -4,10 +4,6 @@
 yaml_files=("qna_configmap_gaudi" "redis-vector-db"  "tei_embedding_gaudi_service" "tei_reranking_service" "tgi_gaudi_service" "retriever" "embedding" "reranking" "llm" "chaqna-xeon-backend-server")
 for element in ${yaml_files[@]}
 do
-    echo "Applying manifest from ${element}.yaml"
-    kubectl apply -f "${element}.yaml"
+    echo "Delete manifest from ${element}.yaml"
+    kubectl delete -f "${element}.yaml"
 done
-
-# scale
-kubectl scale deploy tgi-gaudi-service-deploy --replicas=4
-kubectl scale deploy tei-embedding-gaudi-service-deploy --replicas=2
