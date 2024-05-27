68d0ab5694e0df833bd97d02b61e82959629db9d
leslieluyu <leslie.luyu@gmail.com>
2024-05-27 05:50:27 +0000

    add gaudi(including install_xeon and install_gaudi)

---
+++ genai_solution/GenAIInfra/Demo_ChatQnA/gaudi/install_all_gaudi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Array of YAML file names
+yaml_files=("qna_configmap" "redis-vector-db"  "tei_embedding_gaudi_service" "tei_reranking_service" "tgi_gaudi_service" "retriever" "embedding" "reranking" "llm" "chaqna-xeon-backend-server")
+for element in ${yaml_files[@]}
+do
+    echo "Applying manifest from ${element}.yaml"
+    kubectl apply -f "${element}.yaml"
+done
+
+# scale
+kubectl scale deploy tgi-gaudi-service-deploy --replicas=4
+kubectl scale deploy tei-embedding-gaudi-service-deploy --replicas=2
